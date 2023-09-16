50e99e72d9e28c54afd7215ceeb14b2b42380982
Bruce DuPlanty <bduplanty@gmail.com>
2023-09-15 22:13:00 -0700

    fixed url issues in layout files

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -1,72 +1,72 @@
 
 var documents = [{
     "id": 0,
-    "url": "http://localhost:4000/404.html",
+    "url": "/404.html",
     "title": "404",
     "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "http://localhost:4000/about",
+    "url": "/about",
     "title": "About Me...",
     "body": "I am a lifelong educator, saxophonist, former director of bands, a licensed amateur radio operator since 1988, former BBS operator, dabbler in coding… I am a self-reported amateur, part-time, garage workshop, woodworker. I was primarily power tools before getting the hand tool bug and am now a novice amateur part-time garage workshop hand tool woodworker. I found Paul Sellers and Chris Schwarz on YouTube and am sold on hand tools. "
     }, {
     "id": 2,
-    "url": "http://localhost:4000/archive",
+    "url": "/archive",
     "title": "Archive",
     "body": "    September 2023        Harbor Freight Chisels - Windsor Design Chisels      August 2019        To Excel      February 2017        Deming and Data      June 2016        Everyone Can Draw      March 2016        Arizona State Seal      June 2015        Aglie Development        Retro BBS  "
     }, {
     "id": 3,
-    "url": "http://localhost:4000/categories",
+    "url": "/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 4,
-    "url": "http://localhost:4000/",
+    "url": "/",
     "title": "Home",
     "body": "      Featured:                                                                                                                                                                                                           Harbor Freight Chisels - Windsor Design Chisels                              :               Cheap Chisels - Windsor Chisel Set from Harbor Freight:                                                                                                                                                                       Bruce                                01 Sep 2023                                                                                                                                                                                                                                                                                                                  To Excel                              :               To excel is to continally perform. Not for a moment or moments. But to perform day after day, month after month, and to make that. . . :                                                                                                                                                                       Bruce                                01 Aug 2019                                                                                                                                                    All Stories:                                                                                                     Harbor Freight Chisels - Windsor Design Chisels              :       Cheap Chisels - Windsor Chisel Set from Harbor Freight:                                                                               Bruce                01 Sep 2023                                                                                                                                     To Excel              :       To excel is to continally perform.  Not for a moment or moments.  But to perform day after day, month after month, and to make that uncommon performance look commonplace. :                                                                               Bruce                01 Aug 2019                                                                                                                                     Deming and Data              :       Without data you’re just another person with an opinion. :                                                                               Bruce                22 Feb 2017                                                                                                                                     Everyone Can Draw              :       :                                                                               Bruce                21 Jun 2016                                                                                                                                     Arizona State Seal              :       Arizona State Seal on a building in the capitol complex. :                                                                               Bruce                11 Mar 2016                                                                                                                                     Aglie Development              :       Agile Wiki Agile reference Agile 101 - Agile Alliance HBR Article - Embracing Agile:                                                                               Bruce                12 Jun 2015                                                         &laquo; Prev       1        2      Next &raquo;     "
     }, {
     "id": 5,
-    "url": "http://localhost:4000/robots.txt",
+    "url": "/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 6,
-    "url": "http://localhost:4000/page2/",
+    "url": "/page2/",
     "title": "Home",
     "body": "{% if page. url == “/” %}       Featured:       {% for post in site. posts %}    {% if post. featured == true %}      {% include featuredbox. html %}    {% endif %}  {% endfor %}  {% endif %}       All Stories:         {% for post in paginator. posts %}    {% include postbox. html %}    {% endfor %}              {% include pagination. html %}    "
     }, {
     "id": 7,
-    "url": "http://localhost:4000/cheap_chisels/",
+    "url": "/cheap_chisels/",
     "title": "Harbor Freight Chisels - Windsor Design Chisels",
     "body": "2023/09/01 - Cheap Chisels - Windsor Chisel Set from Harbor FreightAn initial look at the chisels…: About me: I am a self-reported amateur, part-time, garage workshop, woodworker. I was primarily power tools before getting the hand tool bug and am now a novice amateur part-time garage workshop hand tool woodworker. I found Paul Sellers and Chris Schwarz on YouTube and am sold on hand tools. Building a tool collection: For right or wrong, I started to build a tool collection. I started to acquire some bench planes years ago and then took a break because of house moves and a growing family. I recently was ready to start doing some projects and went down the rabbit hole researching workbenches and found Paul Sellers on YouTube, his site, and his blog. Further research led to Chris Schwarz, Lost Art Press, and others. In an effort to build a workbench, namely a Paul Sellers workbench, I felt the need to tune up a No. 4 Stanley Plane I had already acquired and began working on creating the workbench. As I started to think ahead both for the project and future projects, I started to scan eBay for tools I thought I needed or may need. Apparently, I had the antique and tool bug. More on that in a future post. Paul talks a lot about not needing to get the expensive tools, and the latest and greatest, that even a cheap, inexpensive, tool can be used for work and with practice, the willingness to try, that some of these cheap knockoffs, China-produced tools, are okay. He has spoken a lot about the “Aldi Chisels” which at one time were available, but no longer. Chris mentions that Buck Brothers chisels from Home Depot are a good starter set, or acceptable. I was fortunate that I had purchased a small set of those when I bought my first starter home that needed work, As I started to build my workbench, using only hand tools, I purchased more Buck Brothers chisels to round out my set. But they have handles not made of wood, and chopping mortises and levering out the waste proved that these handles are not the most comfortable. Windsor Calling: The magic of the Aldi chisels seems to be that they hand wood handles and were roughly designed for woodworking. They weren’t perfect, but they had good bones. Decent handle, and decent metal that holds an edge to some degree, but they are not as refined as the $45+ chisels (I really mean the $80-90+ per chisel chisels). Here comes Harbor Freight. Their clamps are decent… and they stock the $10 Windsor Chisel Set. Wood handles, nice length, and sizes 1/4”, 1/2”, 3/4”, 1”, 1 1/4” and 1 1/2”… all for $10. The reviews are sketchy. Some talk about the metal and its ability to hold an edge. Some have issues with fit and finish, unsquare, grinding of bevels, and handle cracking out of the package. (Many talk about not being sharp out of the package, but it’s my amateur understanding that no chisel comes truly ready to go, they all need flattening and sharpening) One review, that I give the most weight to, talks about the beveled edge and thickness as it approaches the tip in relation to the ability to clean out a dovetail, these are too thick and too near the tip. With those concerns in mind, I bought two packs. HF has a 90-day return policy on these and my thinking was to go through them picking out the best from each size and return a full set. If I found a cracked handle or badly ground chisel it would be easier than running back and forth to the store and pressing my luck with a full set of chisels each time.  Initial Analysis: With all of that in mind, I hope to provide a little detail about these chisels to aid others in deciding for themselves about getting and using these. I admit that I have never used a professional or high-end chisel and do hope to justify the expense in the future based on my growing skills, time available to do woodworking and a backlog of stuff I want to build. But for now, I have a set of Bucks and some of these HF Windsors lying around, might as well use and abuse these. Opened the first pack carefully, so I could return them if needed, and started to inspect the handles and side bevels, compare them to my Buck Brothers, and check them for square.  First, all of the handles seem to be in good shape. Definitely a bit warmer feeling in the hand than whatever hard compound the Bucks have as a handle. https://www. wonkeedonkeetools. co. uk/media/wysiwyg/11WC-Wood-Chisels-David/11WC04/11WC-4-1. jpg The chisel is longer than my Bucks both overall as well as in most cases the “blade” part as well. Windsor 3/4” in chisel, blade 3 3/4” long, overall around 9 1/2” long compared to the Buck 3/4” blade at 3” long and overall about 8 1/4”.  In comparing the side bevels, the image shows 3/4” size of both brands, the Windsors side bevel indeed haven’t been ground down as much as the Buck Brothers which produces a thicker edge throughout the side all the way down to the tip. At this point, I’m not sure how the extra thickness will impact my work so I can’t really speak to how big of a deal this is. Buck Bros on the left, HF Windsor on the right In checking square, it seems like they are really close if not right on. 3/4” chisel squareness check 1/2” chisel squareness check The second squareness image shows how the side bevels are not ground the same on that chisel, but again, I’m not sure how much that will impact usability and functionality. Next, I went to sharpening. I’m still working on cutting our mortices, so I picked the 3/4” of both and flattened the backs and sharpened the edge. Both have a hollow behind the tip but both took an edge with about the same effort.   In Summary: Overall on the pack I’ve opened the handles seem to be in good shape and the chisels seem to be usably square. My next steps are to put them to use side-by-side with the Bucks and see how they perform and hold up. As one blog post put it, comparing two cheap or low-quality chisels is pointless. Hopefully one day I will obtain some “better” chisels and then be able to experience and speak better to the differences. At this point, I’m focusing on usability to get the job done and the general enjoyment that these allow me to engage in creating and building. …More to follow… "
     }, {
     "id": 8,
-    "url": "http://localhost:4000/to-excel/",
+    "url": "/to-excel/",
     "title": "To Excel",
     "body": "2019/08/01 - To excel is to continally perform.  Not for a moment or moments.  But to perform day after day, month after month, and to make that uncommon performance look commonplace. "
     }, {
     "id": 9,
-    "url": "http://localhost:4000/deming-and-data/",
+    "url": "/deming-and-data/",
     "title": "Deming and Data",
     "body": "2017/02/22 - Without data you’re just another person with an opinion. -W. Edwards Demming, Data Scientist "
     }, {
     "id": 10,
-    "url": "http://localhost:4000/everyonecandraw/",
+    "url": "/everyonecandraw/",
     "title": "Everyone Can Draw",
     "body": "2016/06/21 - "
     }, {
     "id": 11,
-    "url": "http://localhost:4000/arizonastateseal/",
+    "url": "/arizonastateseal/",
     "title": "Arizona State Seal",
     "body": "2016/03/11 - Arizona State Seal on a building in the capitol complex. "
     }, {
     "id": 12,
-    "url": "http://localhost:4000/agile-development/",
+    "url": "/agile-development/",
     "title": "Aglie Development",
     "body": "2015/06/12 - Agile Wiki Agile reference Agile 101 - Agile Alliance HBR Article - Embracing AgileTools:  kanboard - Self Hosted Taiga. io SCRUM tool Monday. comimage: Infographic vector created by freepik - www. freepik. com "
     }, {
     "id": 13,
-    "url": "http://localhost:4000/retro-bbs-site/",
+    "url": "/retro-bbs-site/",
     "title": "Retro BBS",
     "body": "2015/06/03 - Remembering the BBSes of the 80’s.  You can still get the source code and some folks have ported them to telnet access. Retro BBSes Retro up BBS: rdnetbbs. comRetroDigital Current BBS list and info:  telnet BBS Guide Wikipedia entrySource code of retro-style BBS system…: BBS Documentary image: https://arstechnica. com/gadgets/2014/01/modems-warez-and-ansi-art-remembering-bbs-life-at-2400bps/ "
     }];
